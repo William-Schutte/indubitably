1631a09b6479a4110ea2c3c4355b00fe9bb6fb36
William Schutte <williamdschutte@gmail.com>
2020-11-23 11:13:11 -0500

    Init AWS Amplify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
